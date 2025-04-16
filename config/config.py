03751fb871bbb96d4560729c435bc7fb763b2931
theworstofall <165209565+theworstofall@users.noreply.github.com>
2025-04-16 22:57:17 +0545

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,7 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'YOUR_SECRET_KEY')  # Fallback for dev
     SQLALCHEMY_TRACK_MODIFICATIONS = False
