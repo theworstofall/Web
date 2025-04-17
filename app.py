aa7ad8314ab40dd4d3ebe311e00ce08bbd23d0fa
theworstofall <165209565+theworstofall@users.noreply.github.com>
2025-04-17 17:36:50 +0545

    Update app.py

--- app.py
+++ app.py
@@ -273,4 +273,4 @@ def view_contact_submissions():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
